cc600bb9b8db420c04311b98b52033293a51c2b0
Erik Rio Setiawan <erikriosetiawan49@gmail.com>
2020-07-15 21:50:10 +0700

    Create a method to check guitar spec matching

--- src/GuitarSpec.java
+++ src/GuitarSpec.java
@@ -39,4 +39,21 @@ public class GuitarSpec {
     public Wood getTopWood() {
         return topWood;
     }
+
+    public boolean matches(GuitarSpec otherSpec) {
+        if (builder != otherSpec.builder)
+            return false;
+        if ((model != null) && (!model.equals("")) &&
+                (!model.toLowerCase().equals(otherSpec.model.toLowerCase())))
+            return false;
+        if (type != otherSpec.type)
+            return false;
+        if (numStrings != otherSpec.numStrings)
+            return false;
+        if (backWood != otherSpec.backWood)
+            return false;
+        if (topWood != otherSpec.topWood)
+            return false;
+        return true;
+    }
 }
